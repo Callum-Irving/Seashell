3df9c6c94568155b4f459da8fe312fe44652b94d
Callum Irving <callum.irving@protonmail.com>
2022-05-01 17:58:33 -0400

    execute external programs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,42 @@
 mod commands;
 mod parser;
 
+use pathsearch::find_executable_in_path;
 use reedline::{DefaultPrompt, Reedline, Signal};
+use std::process::Command;
+
+#[derive(Default)]
+struct Context {
+    pub last_exit: i32,
+}
 
 fn main() {
     let mut line_editor = Reedline::create();
     let prompt = DefaultPrompt::default();
+    let mut ctx = Context::default();
 
     loop {
         let sig = line_editor.read_line(&prompt).unwrap();
         match sig {
             Signal::Success(buf) => {
                 let tokens = parser::parse_line(&buf);
+                if tokens.is_empty() {
+                    continue;
+                }
+
+                // TODO: Replace variables with their literal values
+
+                // Execute command
                 if let Some(builtin) = commands::get_builtin(tokens[0]) {
                     builtin(&tokens);
+                } else if let Some(path) = find_executable_in_path(tokens[0]) {
+                    let status = Command::new(path)
+                        .args(tokens[1..].iter())
+                        .status()
+                        .expect("error");
+                    ctx.last_exit = status.code().unwrap();
                 } else {
-                    println!("Unknown identifier: {}", tokens[0]);
+                    eprintln!("Seashell error: {} is not in PATH or builtin", tokens[0]);
                 }
             }
             Signal::CtrlD | Signal::CtrlC => {
